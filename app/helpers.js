d658eecd3254dbb777c235c3e0148fed2b26d877
David Cook <divergentdave@gmail.com>
2015-10-17 17:30:34 -0500

    Don't truncate snippets from search results
    Elasticsearch appears to do a good job of staying near the target
    fragment length as it is. Moreover, truncating the fragments isn't safe,
    as it could result in invalid HTML

--- app/helpers.js
+++ app/helpers.js
@@ -5,13 +5,6 @@ var numeral = require("numeral");
 
 module.exports = {
 
-  truncate: function(text, limit) {
-    if (text.length > limit)
-      return text.substr(0, limit);
-    else
-      return text;
-  },
-
   q: function(object) {
     return qs.stringify(object);
   },
