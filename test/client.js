0d2aca331ece528f229042174364f0aa97b1bcb9
David Cook <divergentdave@gmail.com>
2016-01-05 22:27:13 -0600

    Initial boilerplate to get tests set up

---
+++ test/client.js
@@ -0,0 +1,17 @@
+var webdriverio = require('webdriverio');
+var options = {
+  desiredCapabilities: {
+    browserName: 'phantomjs'
+  }
+};
+var client = webdriverio.remote(options);
+
+before(function(done) {
+  client.init(done);
+});
+
+after(function(done) {
+  client.end(done);
+});
+
+exports.client = client;
---
+++ test/homepage.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+
+var client = require('./client').client;
+var server = require('./server');
+
+describe('homepage', function() {
+  it('has a title', function(done) {
+    var baseURL = server.getBaseURL();
+    assert.notEqual(baseURL, undefined);
+    client
+      .url(baseURL)
+      .getTitle(function(err, title) {
+        assert.equal(title, 'Oversight.io | Bringing into sight the US government\'s oversight community.');
+      })
+      .call(done);
+  });
+});
