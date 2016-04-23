47006c571260d1315e00edb64f85f021cdc4be4d
David Cook <divergentdave@gmail.com>
2016-04-22 23:17:59 -0500

    Update branding, per discussion
    https://www.youtube.com/watch?v=V-fRuoMIfpw

--- test/homepage.js
+++ test/homepage.js
@@ -12,7 +12,7 @@ describe('homepage', function() {
     client
       .url(baseURL)
       // .getTitle(function(err, title) {
-      //   assert.equal(title, 'Oversight.garden | Collecting the oversight community\'s work in one place.');
+      //   assert.equal(title, 'Oversight | Collecting the oversight community\'s work in one place.');
       // })
       .call(done);
   });
