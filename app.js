5d30f12d778171accda35000264f91c60f24eb8f
David Cook <divergentdave@gmail.com>
2016-04-24 14:31:39 -0500

    Use Chosen for agency filters mulit-select

--- app.js
+++ app.js
@@ -16,12 +16,18 @@ else
 
 // copy client-side script from dependency
 function copy(fromPath, toPath) {
-  var fromStream = fs.createReadStream(fromPath);
-  var toStream = fs.createWriteStream(toPath);
+  var fromStream = fs.createReadStream(path.join(__dirname, fromPath));
+  var toStream = fs.createWriteStream(path.join(__dirname, toPath));
   fromStream.pipe(toStream);
 }
-copy(path.join(__dirname, 'node_modules/nodep-date-input-polyfill/nodep-date-input-polyfill.dist.js'),
-     path.join(__dirname, 'public/scripts/nodep-date-input-polyfill.dist.js'));
+copy('node_modules/nodep-date-input-polyfill/nodep-date-input-polyfill.dist.js',
+     'public/scripts/nodep-date-input-polyfill.dist.js');
+copy('node_modules/jquery/dist/jquery.min.js',
+     'public/scripts/jquery.min.js');
+copy('node_modules/jquery/dist/jquery.min.map',
+     'public/scripts/jquery.min.map');
+copy('node_modules/jquery/dist/jquery.js',
+     'public/scripts/jquery.js');
 
 // app middleware/settings
 app.engine('.html', require('ejs').__express);
--- public/scripts/search.js
+++ public/scripts/search.js
@@ -33,34 +33,6 @@
     });
   }
 
-  var inspector_filter_list = document.getElementById("inspector-filter-list");
-  var inspector_filter_add = document.getElementById("inspector-filter-add");
-  inspector_filter_add.addEventListener("click", function(e) {
-    // TODO
-    var bubble = document.createElement("li");
-    bubble.appendChild(document.createTextNode("Department of Veterans Affairs"));
-    var deleteButton = document.createElement("div");
-    deleteButton.setAttribute("class", "delete-filter");
-    deleteButton.addEventListener("click", function(e) {
-      inspector_filter_list.removeChild(bubble);
-    });
-    bubble.appendChild(deleteButton);
-    var input = document.createElement("input");
-    input.setAttribute("type", "hidden");
-    input.setAttribute("name", "inspector");
-    input.setAttribute("value", "va");
-    bubble.appendChild(input);
-    inspector_filter_list.appendChild(bubble);
-  });
-
-  var server_side_buttons = document.querySelectorAll(".delete-filter");
-  for (var i = 0; i < server_side_buttons.length; i++) {
-    var button = server_side_buttons[i];
-    button.addEventListener("click", function(e) {
-      button.parentNode.parentNode.removeChild(button.parentNode);
-    });
-  }
-
   var date_filter_clear = document.getElementById("date-filter-clear");
   var date_start = document.getElementById("date-start");
   var date_end = document.getElementById("date-end");
@@ -68,4 +40,6 @@
     date_start.value = "";
     date_end.value = "";
   });
+
+  $(".chosen-select").chosen();
 })();
