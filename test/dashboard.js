dd10bf7ec8390b884ff90421396c4df1363962a9
David Cook <divergentdave@gmail.com>
2016-06-29 20:16:14 -0500

    Rewrite dashboard test in tape

--- test/dashboard.js
+++ test/dashboard.js
@@ -1,13 +1,13 @@
 "use strict";
 
-var assert = require('assert');
+var test = require('tape');
 var fs = require('fs');
 var request = require('request');
 
 var server = require('./server');
 var config = require('../config/config');
 
-before(function(done) {
+test('upload dashboard data', function(t) {
   var url = server.getBaseURL() + "/dashboard/upload";
   var qs = {"secret": config.dashboard.secret};
   var options = {
@@ -20,19 +20,20 @@ before(function(done) {
     }
   };
   request(options, function(error, response, body) {
-    assert.ifError(error);
-    assert.equal(200, response.statusCode);
-    done();
+    t.ifError(error);
+    t.equal(200, response.statusCode);
+    t.end();
   });
 });
 
-describe('dashboard renders without error', function() {
-  it('is OK', function(done) {
+test('dashboard renders without error', function(t) {
+  t.test('is OK', function(t) {
     var baseURL = server.getBaseURL();
     request(baseURL + '/dashboard', function(error, response, body) {
-      assert.ifError(error);
-      assert.equal(200, response.statusCode);
-      done();
+      t.ifError(error);
+      t.equal(200, response.statusCode);
+      t.end();
     });
   });
+  t.end();
 });
