3d9b897cbc2f08ee7ef12f037cc948af1234f536
David Cook <divergentdave@gmail.com>
2016-07-07 00:22:23 -0500

    Revert "Redo callback to fix tests"
    This reverts commit 3b82624777c0e0a76d571f4bcdb865827113355d.

--- app/routes.js
+++ app/routes.js
@@ -159,16 +159,14 @@ module.exports = function(app) {
   });
 
   app.get('/reports/:inspector/:report_id', function(req, res) {
-    boot.refreshCredentials(function() {
-      get(req.params.inspector, req.params.report_id).then(function(result) {
-        res.render("report.html", {
-          report: result._source
-        });
-      }, function(err) {
-        console.log("Nooooo!\n\n" + err);
-        res.status(500);
-        res.render("report.html", {report: null});
+    get(req.params.inspector, req.params.report_id).then(function(result) {
+      res.render("report.html", {
+        report: result._source
       });
+    }, function(err) {
+      console.log("Nooooo!\n\n" + err);
+      res.status(500);
+      res.render("report.html", {report: null});
     });
   });
 
@@ -297,10 +295,12 @@ function parse_search_query(request_query, size) {
 }
 
 function get(inspector, report_id) {
-  return es.get({
-    index: config.elasticsearch.index_read,
-    type: 'reports',
-    id: inspector + '-' + report_id
+  boot.refreshCredentials(function() {
+    return es.get({
+      index: config.elasticsearch.index_read,
+      type: 'reports',
+      id: inspector + '-' + report_id
+    });
   });
 }
 
