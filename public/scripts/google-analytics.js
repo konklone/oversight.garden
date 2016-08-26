0f0120ec4d50cb7f8bd5f91e23c95e32d487aa97
David Cook <divergentdave@gmail.com>
2016-08-25 19:47:36 -0500

    Content Security Policy, other security headers

---
+++ public/scripts/google-analytics.js
@@ -0,0 +1,7 @@
+ga('create', 'UA-252618-18', 'oversight.garden');
+
+// Anonymize user IPs (cut off the last octet).
+ga('set', 'anonymizeIp', true);
+ga('set', 'forceSSL', true);
+
+ga('send', 'pageview');
