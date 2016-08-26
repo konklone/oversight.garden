990a1151be3c83eb5176b7530150d2137062d054
David Cook <divergentdave@gmail.com>
2016-08-25 20:21:05 -0500

    Fix Google Analytics script

--- public/scripts/google-analytics.js
+++ public/scripts/google-analytics.js
@@ -1,3 +1,9 @@
+window.ga = function() {
+  window.ga.q.push(arguments);
+};
+window.ga.q = [];
+window.ga.l = 1 * new Date();
+
 ga('create', 'UA-252618-18', 'oversight.garden');
 
 // Anonymize user IPs (cut off the last octet).
